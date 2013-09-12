b28e3313771251f6b9418ae36d159bf36c490a69
Andrew Kozlov <adkozlov@gmail.com>
2013-09-13 01:23:59 +0400

    remove run

--- deanery/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cmd='/usr/local/mysql/bin/mysql deanery --user=root --password='andyandy''
-
-$cmd < create_db.sql
-$cmd < fill_db.sql 
-
-$cmd -e "select last_name from students natural join persons order by birth_date;"
