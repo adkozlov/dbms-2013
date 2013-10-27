5b08fb6a459bbfae5365aa4f77496fe88c857be8
Andrew Kozlov <adkozlov@gmail.com>
2013-10-28 01:31:31 +0400

    7th done

--- 7/query.sql
+++ 7/query.sql
@@ -24,6 +24,33 @@ CREATE TRIGGER refresh_loserst AFTER UPDATE ON marks
 
 -- DROP FUNCTION IF EXISTS refresh_loserst() CASCADE; -- 6
 
+DROP FUNCTION IF EXISTS check_course() CASCADE; -- 7
+CREATE FUNCTION check_course() RETURNS trigger AS $check_course$ 
+BEGIN
+	IF EXISTS (SELECT group_id FROM grouplists WHERE student_id = NEW.student_id AND group_id NOT IN (SELECT group_id FROM schedule WHERE course_id = NEW.course_id)) THEN	
+		RAISE EXCEPTION 'student % cannot have a mark for % course', NEW.student_id, NEW.course_id;
+	END IF;
+	RETURN NEW;
+END;
+$check_course$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS check_courses ON marks;
+CREATE TRIGGER check_courses BEFORE INSERT OR UPDATE ON marks
+	FOR EACH ROW EXECUTE PROCEDURE check_course();
+
+CREATE TABLE NewPoints ( -- 8
+	student_id INT NOT NULL,
+	course_id INT NOT NULL,
+	points INT CHECK(points BETWEEN 0 AND 100),
+	PRIMARY KEY(student_id, course_id),
+	FOREIGN KEY(student_id) REFERENCES students(student_id),
+	FOREIGN KEY(course_id) REFERENCES courses(course_id)
+);
+
+UPDATE marks SET points = NewPoints.points FROM NewPoints WHERE marks.student_id = NewPoints.student_id AND marks.course_id = NewPoints.course_id;
+
+DROP TABLE NewPoints;
+
 DROP FUNCTION IF EXISTS not_decrease_mark() CASCADE; -- 9
 CREATE FUNCTION not_decrease_mark() RETURNS trigger AS $decrease_mark$ 
 BEGIN
